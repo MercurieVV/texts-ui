9d8ebcf8125c79c30f005917c4ec8f94ce91c5ef
MercurieVV <mercurievv@gmail.com>
2018-10-26 13:53:55 +0300

    Improve some workflow

--- src/main/scala/co/nextwireless/texts/ui/Texts_ui.scala
+++ src/main/scala/co/nextwireless/texts/ui/Texts_ui.scala
@@ -8,7 +8,6 @@ import co.nextwireless.texts.ui.client.swagger.zapi.impl._
 import co.nextwireless.texts.ui.client.swagger.model._
 import hammock.HammockF
 import hammock.js.Interpreter
-import outwatch.dom.dsl.{child, children}
 import outwatch.dom.{Handler, Observable, OutWatch, Sink, dsl}
 import monix.execution.Scheduler.Implicits.global
 
@@ -23,11 +22,14 @@ object Texts_ui {
   def main(args: Array[String]): Unit = {
 
     val flow: IO[Unit] = for {
-      onMenuItemClick <- Handler.create[(PageId, String)]
+      onMenuItemClick <- Handler.create[Item]
+      onTextEdit <- Handler.create[Texts]
+      saveText <- Handler.create[(PageId, String, Texts)]
       ps <- TextsWsApi.pages("mde")
-      textReq <- IO.pure(onMenuItemClick.map(pg => TextsWsApi.texts("mde", pg._1, pg._2)))
+      textReq <- IO.pure(onMenuItemClick.map(pg => TextsWsApi.texts("mde", pg.id._1, pg.id._2)))
       text <- hammockObservableExec(textReq)
-      ns <- IO.pure(createWorkflow(onMenuItemClick, ps, text))
+      ns <- IO.pure(createWorkflow(onMenuItemClick, ps, text, onTextEdit, saveText))
+      st <- IO {saveText.doOnNext(t => println(t)).subscribe()}
       ow <- OutWatch.renderInto("#app", ns)
     } yield ow
 
@@ -44,15 +46,40 @@ object Texts_ui {
     }
   }
 
-  private def createWorkflow(oc: Handler[(PageId, String)], ps: List[Page], texts: Observable[Texts]) = {
+  private def createWorkflow(
+                              textIdO: Handler[Item],
+                              ps: List[Page],
+                              texts: Observable[Texts],
+                              onTextEdit: Handler[Texts],
+                              saveText: Handler[(PageId, String, Texts)]
+                            ) = {
     val listItemsObs = Observable(for {
       page <- ps
       textId <- page.text_ids
-    } yield Item((page.page_id, textId), s"${page.page_id}.$textId")) //ps.flatMap(_.text_ids).map(pg => Item(pg, pg)))
-    val menuItemsView = listItems(listItemsObs, oc)
+    } yield Item((page.page_id, textId), s"${page.page_id}.$textId"))
+
+    val value: Observable[(PageId, String, Texts)] = onTextEdit.zipMap(textIdO)((text, item) => (item.id._1, item.id._2, text))
     <.div(
-      menuItemsView,
-      <.div(child <-- texts.map(t => <.div(t.toString)))
+      listItems(listItemsObs, textIdO),
+      <.div(^.child <-- textIdO.map(_.title)),
+      <.div(<.textArea(^.value <-- texts.map(_.en.getOrElse("").toString), ^.onInput.value --> onTextEdit.transformSink(sk => sk.flatMap(s => texts.map(_.copy(en = Some(s))))))),
+      <.button("Save", ^.onClick(value) --> saveText),
+//      <.button("Save", ^.onClick(onTextEdit.zipMap(oc)((text, page) => (page._1, page._2, text))) --> saveText)
+
+      /*
+            <.div(^.child <-- texts.zip(textIdO)
+              .map(t => {
+                println("list te")
+                <.div(
+                  <.div(<.textArea(t._1.en.getOrElse("").toString, ^.onInput.value --> onTextEdit.mapSink(s => {
+                    val tc = t._1.copy(en = Some(s))
+                    println(tc)
+                    tc
+                  }))),
+                )
+              }),
+            )
+      */
     )
     // menuItemsView
   }
@@ -62,14 +89,17 @@ object Texts_ui {
   val < = dsl.tags
   val ^ = dsl.attributes
 
-  private def listItems(menuItems: Observable[List[Item]], onClick: Sink[(PageId, String)]) = {
-    val value = menuItems.map(items => items.map(item => <.li(
-      ^.onClick(item.id) --> onClick,
-      item.title,
-    )))
+  private def listItems(menuItems: Observable[List[Item]], onClick: Handler[Item]) = {
     <.ul(
-      //      Sstyle.container,
-      children <-- value
+      ^.children <-- menuItems.doOnNext(t => println(t + " gg")).map(items => items.map(item =>
+        <.li(
+          ^.onClick(item) --> onClick.mapSink(f=>{
+            println("xxx")
+            f
+          }),
+          item.title,
+        )
+      ))
     )
   }
 
